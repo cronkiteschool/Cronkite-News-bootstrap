c28f27364ba56b65ab0c3cb60e055a49a2d5c0c7
Jeremy Leggat <jleggat@asu.edu>
2016-11-29 13:30:00 -0700

    change jquery scrolldepth

--- header-election.php
+++ header-election.php
@@ -91,7 +91,7 @@
 	<meta property="fb:pages" content="305166330794" />
 
     <?php wp_head(); ?>
-		<script src="<?php bloginfo('template_directory');?>/js/plugins/jquery.scrolldepth.js"></script>
+<!-- 		<script src="<?php bloginfo('template_directory');?>/js/plugins/jquery.scrolldepth.js"></script> -->
 	 <script>
 	 jQuery(function() {
 		 jQuery.scrollDepth({
