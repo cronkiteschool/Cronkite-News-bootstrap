99e97973d9fa74e9491c65293392f61789529e40
Jeremy Leggat <jleggat@asu.edu>
2016-11-29 13:06:59 -0700

    change jquery.matchHeight

--- functions.php
+++ functions.php
@@ -232,7 +232,7 @@ if (!is_admin()) {
     wp_enqueue_script( 'wow', get_template_directory_uri() . '/js/plugins/wow.js', null, '0.1.6', true );
     wp_enqueue_script( 'slick', get_template_directory_uri() . '/js/plugins/slick.js', null, '1.4.1', true );
     wp_enqueue_script( 'owl-js', get_template_directory_uri() . '/js/plugins/owl.carousel.min.js', null, null, true );
-    wp_enqueue_script( 'matchHeight', get_template_directory_uri() . '/js/plugins/jquery.matchHeight-min.js', null, null, true );
+    wp_enqueue_script( 'matchHeight', get_template_directory_uri() . '/js/plugins/jquery.matchHeight.js', null, '0.5.2', true );
 
 
       //Custom Scripts
